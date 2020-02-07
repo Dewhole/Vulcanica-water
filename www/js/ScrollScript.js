e058dc58992d2a6a1e6ce2d6f999ce75b315e365
Gavrilov Kirill <gavrilovskirill@gmail.com>
2020-02-08 01:41:05 +0700

    PageFull-script for Desktop

--- www/js/ScrollScript.js
+++ www/js/ScrollScript.js
@@ -1,13 +1,46 @@
 'use strict';
 $(document).ready(function() {
-  var scrolling = false
+  var scrolling = false;
+  var isScroll = true;
+  var sectionFullMinSlide = 1;
+  var sectionFullMaxSlide = $('#backgroudnFullPage div.sectionFull').length;
 
     function navigateNextSlide() {
-        console.log("NextSlide")
+        if (isScroll == true) {
+            scrolling = true;
+            isScroll = false;
+            setTimeout(() => {isScroll = true; scrolling = false;}, 1500);
+            var activeSlideID = $('.activeSlide').attr("id").slice(-1);
+            // console.log("activeSlideID", activeSlideID);
+            if(activeSlideID == sectionFullMaxSlide) { 
+                console.log("END"); 
+            } else {
+                $('#screenID'+activeSlideID).removeClass("activeSlide");
+                $('#screenID'+activeSlideID).addClass("pre-activeSlide");
+                var next_activeSlideID = +activeSlideID + 1;
+                $('#screenID'+next_activeSlideID).addClass("activeSlide");
+                $('#screenID'+next_activeSlideID).removeClass("next-activeSlide");
+            }
+        }else{ console.log(isScroll)}
     }
 
     function navigatePrevSlide() {
-        console.log("PrevSlide")
+        if (isScroll == true) {
+            scrolling = true;
+            isScroll = false;
+            setTimeout(() => {isScroll = true; scrolling = false;}, 1500);
+            var activeSlideID = $('.activeSlide').attr("id").slice(-1);
+            // console.log("activeSlideID", activeSlideID);
+            if(activeSlideID == sectionFullMinSlide) { 
+                console.log("START"); 
+            } else {
+                $('#screenID'+activeSlideID).removeClass("activeSlide");
+                $('#screenID'+activeSlideID).addClass("next-activeSlide");
+                var prev_activeSlideID = activeSlideID - 1;
+                $('#screenID'+prev_activeSlideID).addClass("activeSlide");
+                $('#screenID'+prev_activeSlideID).removeClass("pre-activeSlide");
+            }
+        }else{ console.log(isScroll)}
     }
 
     function navigateLeft() {
@@ -18,14 +51,25 @@ $(document).ready(function() {
         console.log("RIGHT")
     }
 
+    //  Предыдущий слайд
+    $('#prevLayout').click(function(){
+        navigatePrevSlide();
+    });
+
+    //  Следующий слайд
+    $('#nextLayout').click(function(){
+        navigateNextSlide();
+    });
+
+
     //mousewheel
     $(document).on("mousewheel DOMMouseScroll", function(event) {
         if (!scrolling) {
             if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-                console.log("-", event.originalEvent.detail)
+                // console.log("-", event.originalEvent.detail)
                 navigateNextSlide();
             } else {
-                console.log("+", event.originalEvent.detail)
+                // console.log("+", event.originalEvent.detail)
                 navigatePrevSlide();
             }
         }
